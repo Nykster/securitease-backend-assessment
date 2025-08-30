26c42fea8617aca4dfcbe75037e2fb8b8a5e18a1
Nykster <39985384+Nykster@users.noreply.github.com>
2025-08-30 15:43:28 -0500

    final edits

--- src/test/java/com/securitease/api/CountriesApiTest.java
+++ src/test/java/com/securitease/api/CountriesApiTest.java
@@ -22,40 +22,36 @@ public class CountriesApiTest {
         given()
             .queryParam("fields", "name,languages")
         .when()
-            .get("/all")
+            .get("/all") // HTTP GET request to /all endpoint
         .then()
             .assertThat()
-            .statusCode(200)
-            .body(matchesJsonSchemaInClasspath("countries-schema.json"));
+            .statusCode(200) // confirmation request has been successful (HTTP 200 OK)
+            .body(matchesJsonSchemaInClasspath("countries-schema.json")); // response body compared to my derived schema
     }
 
-    //Scenario 2: Confirm the total number of countries returned by the API.
+    //Scenario 2: Confirmation of total number of countries returned by the API.
     @Test
     public void testTotalNumberOfCountries() {
         given()
-            .queryParam("fields", "cca2")
+            .queryParam("fields", "cca2") // return only two-letter country code for each entry
         .when()
-            .get("/all")
+            .get("/all") // use the /all endpoint
         .then()
             .assertThat()
             .statusCode(200)
-            .body("size()", equalTo(250));
+            .body("size()", equalTo(250)); // assert that the number of countries returned equals 250
     }
     
     //Scenario 3: Validate that South African Sign Language is an official language of South Africa.
     @Test
     public void testSouthAfricaIncludesSignLanguage() {
         given()
-            // Use a path parameter to search for a specific country by name.
-            .pathParam("name", "South Africa")
+            .pathParam("name", "South Africa") // path parameter searches for a specific country by name
         .when()
-            // Use the /name/{name} endpoint.
-            .get("/name/{name}")
+            .get("/name/{name}") // use the /name/{name} endpoint
         .then()
             .assertThat()
             .statusCode(200)
-            // GPath expression to get the list of language values from the first result.
-            // Assert that the list of languages has the item "South African Sign Language".
-            .body("[0].languages.values()", hasItem("South African Sign Language"));
+            .body("[0].languages.values()", hasItem("South African Sign Language")); // assert returned list of languages has item "South African Sign Language"
     }
 }
